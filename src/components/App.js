6e306a54fe23bddb39ca43c819378c670362de79
pedro218 <pedroaf218@gmail.com>
2021-06-06 18:42:22 -0700

    Initialize code and set up bootstrap

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Container } from 'react-bootstrap'
+
+const App = () => {
+  return <Container>This will be a todo list App</Container>
+}
+
+export default App
+
+// import 'bootstrap/dist/css/bootstrap.min.css'
